4c6b19da299c1050c1477be8eb906b0caae5c2ac
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-09-21 19:44:30 -0500

    renames config file and refactors to more kotlin-esque syntax

--- src/main/kotlin/com/jocampo/heroes/medallion/booker/config/JacksonConfig.kt
+++ src/main/kotlin/com/jocampo/heroes/medallion/booker/config/JacksonConfig.kt
@@ -2,13 +2,12 @@ package com.jocampo.heroes.medallion.booker.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.jocampo.heroes.medallion.booker.controllers.HeroesController
-import com.jocampo.heroes.medallion.booker.services.HotSAPIClient
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class BeanConfig {
+class JacksonConfig {
     @Bean
-    fun objectMapper(): ObjectMapper = jacksonObjectMapper()
+    fun objectMapper(): ObjectMapper = jacksonObjectMapper().apply { this.registerKotlinModule() }
 }

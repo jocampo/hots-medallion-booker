5ca1f88dd6ed9d6edd22fa7a0f1f170f52ee28a1
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-09-23 00:24:27 -0500

    fixes cors issue

--- src/main/kotlin/com/jocampo/heroes/medallion/booker/config/WebSocketConfig.kt
+++ src/main/kotlin/com/jocampo/heroes/medallion/booker/config/WebSocketConfig.kt
@@ -1,5 +1,6 @@
 package com.jocampo.heroes.medallion.booker.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.jocampo.heroes.medallion.booker.services.WebSocketRPCService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -10,12 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
-class WebSocketConfig : WebSocketConfigurer {
+class WebSocketConfig(
+        private val objectMapper: ObjectMapper
+) : WebSocketConfigurer {
     private val logger: Logger = LoggerFactory.getLogger(WebSocketConfig::class.java)
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         val wsPath = "/rooms"
         logger.info("Registering ws handler for $wsPath")
-        registry.addHandler(WebSocketRPCService(), wsPath).withSockJS()
+        registry.addHandler(WebSocketRPCService(objectMapper), wsPath)
+                .setAllowedOrigins("*")
+                .withSockJS()
     }
 }

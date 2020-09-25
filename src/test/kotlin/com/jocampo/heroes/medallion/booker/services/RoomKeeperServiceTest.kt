904e1dae72361a00642b2bb5d5043c4f823848d2
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-09-25 01:58:06 -0500

    Finish up tests for RKS (still non-granular, but all bases are covered)

--- src/test/kotlin/com/jocampo/heroes/medallion/booker/services/RoomKeeperServiceTest.kt
+++ src/test/kotlin/com/jocampo/heroes/medallion/booker/services/RoomKeeperServiceTest.kt
@@ -61,7 +61,59 @@ internal class RoomKeeperServiceTest {
     @Test
     fun vacateRoom() {
         val user = User(1, "TestUser")
+        val anotherUser = User(2, "TestUser2")
+
+        val rooms = roomKeeperService.getRooms()
+        assertThat(rooms).isEmpty()
+        val roomCode = roomKeeperService.bookRandomRoom(user)
+        assertThat(rooms.size).isEqualTo(1)
+
+        assertThatExceptionOfType(Exception::class.java).isThrownBy {
+            roomKeeperService.vacateRoom("invalidRoomCode", user)
+        }
+        assertThatExceptionOfType(Exception::class.java).isThrownBy {
+            roomKeeperService.vacateRoom(roomCode, anotherUser)
+        }
+
+        roomKeeperService.joinExistingRoom(roomCode, anotherUser)
+        assertThat(rooms[roomCode]?.users?.size).isEqualTo(2)
+        // User ownership was handed over to anotherUser
+        assertThat(roomKeeperService.vacateRoom(roomCode, user)).isEqualTo(anotherUser.id)
+        assertThat(rooms.size).isEqualTo(1)
+        assertThat(rooms[roomCode]?.users?.size).isEqualTo(1)
+
+        // Vacate the last user
+        assertThat(roomKeeperService.vacateRoom(roomCode, anotherUser)).isNull()
+        assertThat(rooms).isEmpty()
+    }
+
+    @Test
+    fun joinExistingRoom() {
+        val user = User(1, "TestUser")
+        val anotherUser = User(2, "TestUser2")
+
         val rooms = roomKeeperService.getRooms()
         val roomCode = roomKeeperService.bookRandomRoom(user)
+
+        assertThat(roomKeeperService.joinExistingRoom(roomCode, anotherUser)).isTrue()
+        assertThatExceptionOfType(Exception::class.java).isThrownBy {
+            roomKeeperService.joinExistingRoom("invalidRoomCode", anotherUser)
+        }
+
+        val yetAnotherUser = User(3, "TestUser3")
+        assertThatExceptionOfType(Exception::class.java).isThrownBy {
+            roomKeeperService.joinExistingRoom("invalidRoomCode", yetAnotherUser)
+        }
+        assertThat(roomKeeperService.joinExistingRoom(roomCode, yetAnotherUser)).isTrue()
+
+        val user4 = User(4, "TestUser4")
+        val user5 = User(5, "TestUser5")
+        assertThat(roomKeeperService.joinExistingRoom(roomCode, user4)).isTrue()
+        assertThat(roomKeeperService.joinExistingRoom(roomCode, user5)).isTrue()
+
+        val user6 = User(6, "TestUser6")
+        assertThat(roomKeeperService.joinExistingRoom(roomCode, user6)).isFalse()
+        assertThat(rooms[roomCode]?.users?.size).isEqualTo(5)
+
     }
 }

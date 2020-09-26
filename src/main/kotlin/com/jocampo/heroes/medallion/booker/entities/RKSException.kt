a8f07b60728c0a8cc733f65cd1163372f52117a5
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-09-26 18:39:53 -0500

    adds more RKS Error Codes

--- src/main/kotlin/com/jocampo/heroes/medallion/booker/entities/RKSException.kt
+++ src/main/kotlin/com/jocampo/heroes/medallion/booker/entities/RKSException.kt
@@ -7,5 +7,8 @@ enum class RKSErrorCodes(val code: String) {
     ROOM_DOES_NOT_EXIST("0100"),
     ROOM_IS_FULL("0101"),
     USER_ALREADY_IN_ROOM("0102"),
-    USER_NOT_IN_ROOM("0103")
+    USER_NOT_IN_ROOM("0103"),
+    TEAM_IS_FULL("0104"),
+    HERO_ALREADY_IN_TEAM("0105"),
+    HERO_NOT_IN_TEAM("0106")
 }

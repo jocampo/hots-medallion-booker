46f59afab7c4289635892e61f242efe48b92174e
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-09-26 20:11:47 -0500

    Test Utils!

--- src/test/kotlin/com/jocampo/heroes/medallion/booker/services/RoomKeeperServiceTest.kt
+++ src/test/kotlin/com/jocampo/heroes/medallion/booker/services/RoomKeeperServiceTest.kt
@@ -1,6 +1,9 @@
 package com.jocampo.heroes.medallion.booker.services
 
+import com.jocampo.heroes.medallion.booker.entities.RKSException
+import com.jocampo.heroes.medallion.booker.entities.Teams
 import com.jocampo.heroes.medallion.booker.entities.User
+import com.jocampo.heroes.medallion.booker.utils.TestUtils
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
@@ -44,6 +47,8 @@ internal class RoomKeeperServiceTest {
         assertThat(rooms[roomCode]?.ownerId).isEqualTo(user.id)
         assertThat(rooms[roomCode]?.users?.size).isEqualTo(1)
         assertThat(rooms[roomCode]?.users?.elementAt(0)).isEqualTo(user)
+        assertThat(rooms[roomCode]!!.blueTeam).isEmpty()
+        assertThat(rooms[roomCode]!!.redTeam).isEmpty()
     }
 
     @Test
@@ -115,5 +120,49 @@ internal class RoomKeeperServiceTest {
         assertThat(roomKeeperService.joinExistingRoom(roomCode, user6)).isFalse()
         assertThat(rooms[roomCode]?.users?.size).isEqualTo(5)
 
+    }
+
+    @Test
+    fun addHeroToRoom() {
+        val user = User(1, "TestUser")
+
+        val rooms = roomKeeperService.getRooms()
+        val roomCode = roomKeeperService.bookRandomRoom(user)
+        roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Abathur"), Teams.BLUE.side)
+        assertThat(rooms[roomCode]!!.blueTeam.size).isEqualTo(1)
+        assertThatExceptionOfType(RKSException::class.java).isThrownBy {
+            roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Abathur"), Teams.BLUE.side)
+        }
+        roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Blaze"), Teams.BLUE.side)
+        roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Cassia"), Teams.BLUE.side)
+        roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Raynor"), Teams.BLUE.side)
+        roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Qhira"), Teams.BLUE.side)
+        assertThatExceptionOfType(RKSException::class.java).isThrownBy {
+            roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Nope"), Teams.BLUE.side)
+        }
+        roomKeeperService.addHeroToRoom(roomCode, TestUtils.generateDummyHero("Abathur"), Teams.RED.side)
+        assertThat(rooms[roomCode]!!.redTeam.size).isEqualTo(1)
+    }
+
+    @Test
+    fun removeHeroFromRoom() {
+        val user = User(1, "TestUser")
+
+        val rooms = roomKeeperService.getRooms()
+        val roomCode = roomKeeperService.bookRandomRoom(user)
+        val abathur = TestUtils.generateDummyHero("Abathur")
+        val blaze = TestUtils.generateDummyHero("Blaze")
+        roomKeeperService.addHeroToRoom(roomCode, abathur, Teams.BLUE.side)
+        roomKeeperService.addHeroToRoom(roomCode, blaze, Teams.RED.side)
+        roomKeeperService.removeHeroFromRoom(roomCode, abathur, Teams.BLUE.side)
+        assertThat(rooms[roomCode]!!.blueTeam).isEmpty()
+        assertThatExceptionOfType(RKSException::class.java).isThrownBy {
+            roomKeeperService.removeHeroFromRoom(roomCode, abathur, Teams.BLUE.side)
+        }
+        assertThat(rooms[roomCode]!!.blueTeam).isEmpty()
+        assertThatExceptionOfType(RKSException::class.java).isThrownBy {
+            roomKeeperService.removeHeroFromRoom(roomCode, abathur, Teams.RED.side)
+        }
+        assertThat(rooms[roomCode]!!.redTeam.size).isEqualTo(1)
     }
 }
